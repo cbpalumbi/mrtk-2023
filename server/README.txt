eec2c5c713eff52f783c42bb27d4697e14fe2569
Bella Palumbi <christabellapalumbi@gmail.com>
2023-01-13 12:22:41 -0500

    Initial commit

---
+++ server/README.txt
@@ -0,0 +1,3 @@
+nodemon index.js to start
+
+in Unity press play and then space to send msg back and forth
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 8080 },()=>{
+    console.log('server started')
+})
+wss.on('connection', function connection(ws) {
+   ws.on('message', (data) => {
+      console.log('data received \n %o',data)
+      ws.send(data.toString() + ' test');
+   })
+})
+wss.on('listening',()=>{
+   console.log('listening on 8080')
+})
